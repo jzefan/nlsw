42e460119fbce47072998be5f5f6c5df16e051fe
jzefan <jzefan@gmail.com>
2020-03-17 23:33:55 +0800

    Add order plan model

---
+++ controllers/order_plan.js
@@ -0,0 +1,37 @@
+/**
+ * date: 2020/03/17
+ * author: jzefan
+ */
+"use strict";
+
+var OrderPlan = require('../models/OrderPlan');
+//var Bill = require('../models/Bill');
+//var utils = require('./utils');
+
+var bunyan = require('bunyan');
+var logger = bunyan.createLogger({
+  name: 'XHT',
+  streams: [ { level: 'info', stream: process.stdout }, { level: 'error', path: 'app.log' } ]
+});
+
+
+exports.createOrderPlan = function (req, res) {
+  if (req.user.privilege[0] === '0' && req.user.privilege[1] === '0') {
+    res.status.render(404);
+  }
+  else {
+    res.render('order/create_plan', {
+      title: '提单管理',
+      curr_page: '新建提单',
+      curr_page_name: '新建',
+      bShowDataTable: true,
+      dDataDict: data,
+      scripts: [
+        '/js/plugins/select2/select2.min.js',
+        '/js/plugins/select2/select2_locale_zh-CN.js',
+        '/js/plugins/datatables/jquery.dataTables.min.js',
+        '/js/bill_import_create.js'
+      ]
+    });
+  }
+};
